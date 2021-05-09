d84b0c6f2e0a2ae4072c6fb7ce329ed757661759
Rob <robwilliams.email@gmail.com>
2021-05-09 17:51:16 +0100

    -madded fontawesome support

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,17 +2,33 @@ import React from "react";
 import * as styles from "./layout.module.css";
 import NavBar from "./nav-bar";
 import NavBarItem from "./nav-bar-item";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFacebookSquare, faTwitterSquare, faInstagramSquare, faTwitter, faFacebook, faInstagram, faStrava } from "@fortawesome/free-brands-svg-icons";
+
 
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: "0", width: "1024px", padding: `0`, margin: "auto" }}>
-      <div className={styles.banner}>
-        <NavBar>
-          <NavBarItem name="Home" />
-          <NavBarItem name="About" />
-        </NavBar>
+    <div style={{display:"flex", flexDirection:"column"}}>
+      <div className={styles.container}>
+        <div className={styles.banner}>
+          <NavBar>
+            <NavBarItem name="HOME" />
+            <NavBarItem name="ABOUT" />
+            <FontAwesomeIcon icon={faTwitter} size="2x"/>
+            <FontAwesomeIcon icon={faFacebook} size="2x"/>
+            <FontAwesomeIcon icon={faInstagram} size="2x"/>
+            <FontAwesomeIcon icon={faStrava} size="2x"/>
+
+          </NavBar>
+        </div>
       </div>
-      {children}
+      <main>
+        {children}
+      </main>
+      <footer>
+        
+      </footer>
     </div>
+
   );
 }
